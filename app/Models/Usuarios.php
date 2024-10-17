65e6d43e29f4024bf90001cd4991cc7c9b0dee9c
Veru7680 <veru6198@gmail.com>
2024-10-17 16:54:09 -0400

    se configuro Usuario register y problemas con Login

---
+++ app/Models/Usuarios.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuarios extends Model
+{
+    use HasFactory;
+    protected $table = 'Usuarios';
+    protected $fillable=[
+        'nombre',
+        'email',
+        'contraseña',
+        'rol'
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Request\Auth\LoginRequest;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
